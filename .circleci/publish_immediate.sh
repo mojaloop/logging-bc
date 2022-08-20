0df7b89ea4e227cce40bc37bac655ed870d96084
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2022-08-20 20:31:19 +0100

    circleci tests

--- .circleci/publish_immediate.sh
+++ .circleci/publish_immediate.sh
@@ -21,19 +21,27 @@ fi
 PACKAGES=$(ls ${ROOT} -l | grep ^d | awk '{print $9}')
 echo "Searching for changes ..."
 
-PUBLISH_TARGETS=""
+PUBLISH_PACKAGES=""
 COUNT=0
 
 for PACKAGE in ${PACKAGES[@]}
 do
   PACKAGE_PATH=${ROOT}/$PACKAGE
   PACKAGE_LAST_CHANGE_COMMIT_SHA=$(git --no-pager log -1 --format=format:%H --full-diff $PACKAGE_PATH)
-  PACKAGE_IS_PRIVATE=$(jq -e ".private == true" $PACKAGE_PATH/package.json)
+
+  # jq not always installed
+  # PACKAGE_IS_PRIVATE=$(jq -e ".private == true" $PACKAGE_PATH/package.json)
+
+  PACKAGE_IS_PRIVATE=$(cat $PACKAGE_PATH/package.json | grep private | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
+  if [[ -z "$PACKAGE_IS_PRIVATE" ]]; then
+    PACKAGE_IS_PRIVATE=false
+  fi
+
   echo -e "Package: ${PACKAGE} last change commit sha is: ${PACKAGE_LAST_CHANGE_COMMIT_SHA} (Private: ${PACKAGE_IS_PRIVATE})"
 
-  if [[ "$PACKAGE_IS_PRIVATE" == 'false' ]] && [[ -z "$PACKAGE_LAST_CHANGE_COMMIT_SHA" ]] || [[ "$CIRCLE_SHA1" == "$PACKAGE_LAST_CHANGE_COMMIT_SHA" ]]; then
+  if [[ -z "$PACKAGE_IS_PRIVATE" || "$PACKAGE_IS_PRIVATE" == 'false' ]] && [[ -z "$PACKAGE_LAST_CHANGE_COMMIT_SHA" ]] || [[ "$CIRCLE_SHA1" == "$PACKAGE_LAST_CHANGE_COMMIT_SHA" ]]; then
         PARAMETERS+=", \"$PACKAGE\":true"
-        PUBLISH_TARGETS+"$PACKAGE "
+        PUBLISH_PACKAGES+"$PACKAGE "
         COUNT=$((COUNT + 1))
         #echo -e "\e[36m  [+] ${PACKAGE} \e[21m (changed in [${LATEST_COMMIT_SINCE_LAST_BUILD:0:7}])\e[0m"
         echo -e "Package: ${PACKAGE} CHANGED!! Publishing starting..."
@@ -63,6 +71,6 @@ if [[ $COUNT -eq 0 ]]; then
   exit 0
 fi
 
-echo "Changes detected in ${COUNT} package(s): ${PUBLISH_TARGETS}"
+echo "Changes detected in ${COUNT} package(s): ${PUBLISH_PACKAGES}"
 exit 0
 
