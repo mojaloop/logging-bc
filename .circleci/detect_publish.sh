513cd153fb80e0ab43d3bd1e8180690302f4417a
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2022-08-20 19:39:33 +0100

    ci tests

--- .circleci/detect_publish.sh
+++ .circleci/detect_publish.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -x
 
+############################################
+## 2. Setup
+############################################
+
 # The root directory of packages to publish
 ROOT="./modules"
 REPOSITORY_TYPE="github"
 CIRCLE_API="https://circleci.com/api"
-
-if [[ -z "${CIRCLE_TOKEN}" ]]; then
-    echo -e "\e[93mEnvironment variable CIRCLE_TOKEN is not set. Exiting.\e[0m"
-  exit 1
-fi
 
 if [[ -z "${CIRCLE_SHA1}" ]]; then
     echo -e "\e[93mEnvironment variable CIRCLE_SHA1 is not set. Exiting.\e[0m"
@@ -22,8 +21,6 @@ fi
 PACKAGES=$(ls ${ROOT} -l | grep ^d | awk '{print $9}')
 echo "Searching for changes ..."
 
-## The CircleCI API parameters object
-PARAMETERS='"trigger":false'
 PUBLISH_TARGETS=""
 COUNT=0
 
@@ -53,25 +50,3 @@ fi
 echo "Changes detected in ${COUNT} package(s): ${PUBLISH_TARGETS}"
 
 
-
-############################################
-## 3. CicleCI REST API call
-############################################
-#DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
-DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": {  \"trigger\":false, \"publish\":true, \"publish_targets\":\"${PUBLISH_TARGETS}\" } }"
-echo "Triggering pipeline with data:"
-echo -e "  $DATA"
-
-URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
-HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
-
-if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
-    echo "API call succeeded."
-    echo "Response:"
-    cat response.txt
-else
-    echo -e "\e[93mReceived status code: ${HTTP_RESPONSE}\e[0m"
-    echo "Response:"
-    cat response.txt
-    exit 1
-fi
